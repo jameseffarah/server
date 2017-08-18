1127709ba6f907df254b1402ca0743ac15531a47
James Effarah <jeffarah@me.com>
2017-08-18 13:57:32 -0600

    section 126

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,8 +1,20 @@
-const requireLogin = require('../middlewares/requireLogin'),
-	  requireCredits = require('../middlewares/requireCredits');
+const mongoose 			= require('mongoose'),
+	  requireLogin 		= require('../middlewares/requireLogin'),
+	  requireCredits 	= require('../middlewares/requireCredits');
+
+const Survey = mongoose.model('survey');
 
 module.exports = app => {
-	app.post('/api/surveys', requireLogin, (req, res) => {
+	app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+		const { title, subject, body, recipients } = req.body;
 
+		const survey = new Survey({
+			title,
+			subject,
+			body,
+			recipients: recipients.split(',').map(email => ({ email.trim()})),
+			_user: req.user.id,
+			dateSent:; Date.now()
+		});
 	});
-}
+};
